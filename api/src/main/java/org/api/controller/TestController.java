a0bba3c8aa930719d72367cbf8c487a2cf1361d3
vikasyadavnsit <vickytauruss@gmail.com>
2020-01-17 21:49:55 +0530

    SLF4J Logger Added with custom control

--- api/src/main/java/org/api/controller/TestController.java
+++ api/src/main/java/org/api/controller/TestController.java
@@ -1,6 +1,8 @@
 package org.api.controller;
 
 import org.api.configuration.ApiConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test")
 public class TestController {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Autowired
 	private Environment env;
