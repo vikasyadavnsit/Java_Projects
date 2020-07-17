17b83f293e0c3f36da3c5cac5956496553e8bc72
vikasyadavnsit <vickytauruss@gmail.com>
2020-07-17 20:34:19 +0530

    New Spring Boot JDK14 Based project

---
+++ springBootJDK14/src/main/java/com/battleramp/springBootJDK14/SpringBootJdk14Application.java
@@ -0,0 +1,13 @@
+package com.battleramp.springBootJDK14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdk14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJdk14Application.class, args);
+	}
+
+}
---
+++ springBootJDK14/src/test/java/com/battleramp/springBootJDK14/SpringBootJdk14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.battleramp.springBootJDK14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJdk14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
