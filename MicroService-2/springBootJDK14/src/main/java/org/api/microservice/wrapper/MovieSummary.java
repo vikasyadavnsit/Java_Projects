790a16aad4ceab718425667c68b1b2e7b3a98b78
vikasyadavnsit <vickytauruss@gmail.com>
2020-09-17 19:46:16 +0530

    Config Server Changes

---
+++ MicroService-2/springBootJDK14/src/main/java/org/api/microservice/wrapper/MovieSummary.java
@@ -0,0 +1,11 @@
+package org.api.microservice.wrapper;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MovieSummary {
+	private String title;
+	private String overview;
+}
