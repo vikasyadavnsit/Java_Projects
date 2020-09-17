97797eb3deef260bbe0e0d2fed92ff0867116de2
vikasyadavnsit <vickytauruss@gmail.com>
2020-09-17 21:00:15 +0530

    Config Changes

--- Microservice Config Server/api/src/main/java/org/api/BootApplication.java
+++ Microservice Config Server/api/src/main/java/org/api/BootApplication.java
@@ -2,8 +2,10 @@ package org.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class BootApplication {
 
 	public static void main(String[] args) {
