1fcb51b4d61b373e1710c721c09f3c494aa04f9c
vikasyadavnsit <vickytauruss@gmail.com>
2020-09-17 22:07:44 +0530

    Microservices Dynamic Configurations Done

--- MicroService-1/springBootJDK14/src/main/java/org/api/microservice/controller/ExternalConfigurationController.java
+++ MicroService-1/springBootJDK14/src/main/java/org/api/microservice/controller/ExternalConfigurationController.java
@@ -5,12 +5,14 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
+@RestController
+@RefreshScope
 @RequestMapping("/external-configuration")
 public class ExternalConfigurationController {
 
@@ -28,7 +30,7 @@ public class ExternalConfigurationController {
 
 	@Autowired
 	private DBSettingConfigurationProperties dbSettings;
-	
+
 	@Autowired
 	private Environment env;
 
@@ -41,6 +43,5 @@ public class ExternalConfigurationController {
 	public String environmentDetails() {
 		return env.getActiveProfiles().toString();
 	}
-	
-}
 
+}
