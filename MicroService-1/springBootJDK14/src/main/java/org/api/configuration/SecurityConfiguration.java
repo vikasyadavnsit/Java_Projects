f76e32f39f452ddf6197b3102285eb8258d7aca5
vikasyadavnsit <vickytauruss@gmail.com>
2020-10-25 11:24:40 +0530

    H2 Database Configuration ( Different Modes )

--- MicroService-1/springBootJDK14/src/main/java/org/api/configuration/SecurityConfiguration.java
+++ MicroService-1/springBootJDK14/src/main/java/org/api/configuration/SecurityConfiguration.java
@@ -30,6 +30,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests().antMatchers("/**").permitAll();
+		http.headers().frameOptions().sameOrigin();
 //		http.authorizeRequests().antMatchers("/admin").hasRole("ADMIN").antMatchers("/user").hasAnyRole("USER", "ADMIN")
 //				.antMatchers("/", "/static/**").permitAll().and().formLogin();
 //
