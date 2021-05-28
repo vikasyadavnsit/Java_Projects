9d47e3804255e92ea681145e21bc93c928fdd75f
vikasyadavnsit <vickytauruss@gmail.com>
2021-05-28 13:54:03 +0530

    Docker Microservices started

--- MicroService-1/springBootJDK14/src/main/java/org/api/test/TestClass.java
+++ MicroService-1/springBootJDK14/src/main/java/org/api/test/TestClass.java
@@ -1,45 +1,20 @@
 package org.api.test;
 
-import java.util.Deque;
-import java.util.LinkedList;
-
 public class TestClass {
 
 	public static void main(String[] args) {
+		String a = "qwertrwqwerwqwerwerwqwe";
+		int carr[] = new int[26];
 
-		int n = 10, k = 5;
-		int[] arr = new int[] { 1, 8, 3, 4, 5, 2, 9, 1, 5, 0 };
-		int min = Integer.MAX_VALUE;
-		int max = Integer.MIN_VALUE;
-
-//		for (int i = 0; i < n; i++) {
-//			for (int j = i; j < i + k && j < n; j++) {
-//				min = Math.min(min, arr[j]);
-//				max = Math.max(max, min);
-//			}
-//		}
-
-		Deque<Integer> q = new LinkedList<>();
-		q.add(arr[0]);
-
-		for (int i = 1; i < k; i++) {
-			while (!q.isEmpty() && q.peekLast() > arr[i]) {
-				q.removeLast();
-			}
-			if (q.isEmpty() || q.peekLast() < arr[i])
-				q.add(arr[i]);
-		}
-		max = Math.max(max, q.peekFirst());
-
-		for (int i = k; i < n; i++) {
-			while (!q.isEmpty() && q.peekLast() > arr[i]) {
-				q.removeLast();
-			}
-			if (q.isEmpty() || q.peekLast() < arr[i])
-				q.add(arr[i]);
-			max = Math.max(max, q.peekFirst());
+		for (int i = 0; i < a.length(); i++) {
+			++carr[a.charAt(i) - 97];
 		}
 
-		System.out.println(max);
+		int o = 0;
+		for (int i = 0; i < 26; i++) {
+		}
+
+		System.out.println(o > 1 ? false : true);
+
 	}
 }
