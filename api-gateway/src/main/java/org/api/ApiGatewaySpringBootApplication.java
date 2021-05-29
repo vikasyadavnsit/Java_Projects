f39cb15b9482468e01812686ab1d9b8259ff2481
vikasyadavnsit <vickytauruss@gmail.com>
2021-05-29 15:04:05 +0530

    Reconfigured Configurations

--- api-gateway/src/main/java/org/api/ApiGatewaySpringBootApplication.java
+++ api-gateway/src/main/java/org/api/ApiGatewaySpringBootApplication.java
@@ -3,15 +3,10 @@ package org.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-<<<<<<< HEAD
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableEurekaClient
 @EnableZuulProxy
-=======
-
-@EnableEurekaClient
->>>>>>> c34771a9c456b15f20c99bdc87b0a106c75d3180
 @SpringBootApplication
 public class ApiGatewaySpringBootApplication {
 	public static void main(String[] args) {
